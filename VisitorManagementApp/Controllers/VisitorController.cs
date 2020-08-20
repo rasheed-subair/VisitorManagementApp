af747c2a7e25141f62015219a0785e940b95108a
rasheed <subairolar@gmail.com>
2020-08-20 12:43:55 +0100

    Created CHeckout for Visitors

--- VisitorManagementApp/Controllers/VisitorController.cs
+++ VisitorManagementApp/Controllers/VisitorController.cs
@@ -30,6 +30,17 @@ namespace VisitorManagementApp.Controllers
 
 
         /***********************************************/
+        /*           Visitor Checkout List             */
+        /***********************************************/    //Added this
+        public ActionResult CheckoutList()
+        {
+            var visitor = db.VisitorTable.ToList();
+            return View(visitor);
+        }
+
+
+
+        /***********************************************/
         /*      Visitor Details viewed By admin        */
         /***********************************************/
         public ActionResult Details(int? id)
@@ -73,12 +84,44 @@ namespace VisitorManagementApp.Controllers
                  * Use this instead when success view page is set up
                  return RedirectToAction("Success", "Keycode");
                  */
-                return RedirectToAction("Index");
+                ModelState.Clear();
+                ViewBag.Message = "You have checked in successfully.";
+                
             }
 
             return View();
         }
 
+
+        //Added this - to be edited
+        public ActionResult Checkout(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visitor visitor = db.VisitorTable.Find(id);
+            if (visitor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visitor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Checkout([Bind(Include = "VisitorId,FirstName,LastName,Email,Phone,Address,Purpose,TimeIn,TimeOut")] Visitor visitor)
+        {
+            if (ModelState.IsValid)
+            {
+                visitor.TimeOut = DateTime.Now.ToString();
+                db.Entry(visitor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("CheckoutList");
+            }
+            
+            return View(visitor);
+        }
 
 
         protected override void Dispose(bool disposing)
